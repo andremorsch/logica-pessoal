06f257c011974c39ff08954355c72ff5e3fde126
andremorsch <andremorsch37@gmail.com>
2022-04-12 13:33:22 -0300

    feat: removedor de vogais

---
+++ desafios/001removeVowels.js
@@ -0,0 +1,18 @@
+// Desafio: 
+// Trolls are attacking your comment section!
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+// Note: for this kata y isn't considered a vowel.
+
+const str = "This website is for losers LOL!";
+const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+let response = '';
+
+for(let ind = 0; ind < str.length; ind++) {
+    if(!vowels.includes(str[ind])) {
+        response += str[ind];
+    }
+}
+
+console.log(response);
