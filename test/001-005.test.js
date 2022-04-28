30b4ce34be3db90bf0b41d125b80946924571e75
andremorsch <andremorsch37@gmail.com>
2022-04-28 20:09:27 -0300

    feat: teste apra desafio 002

--- test/001-005.test.js
+++ test/001-005.test.js
@@ -1,8 +1,7 @@
 const { removeVowels } = require('../desafios/001removeVowels');
+const { uniqueInOrder } = require('../desafios/002uniqueInOrder');
 
 describe("Teste exercício 001", () => {
-  const teste01 = "Ths wbst s fr lsrs LL!";
-  
   it('Testa caso correto 1', () => {
     const use01 = removeVowels("This website is for losers LOL!");
     expect(use01).toEqual("Ths wbst s fr lsrs LL!");
@@ -11,5 +10,13 @@ describe("Teste exercício 001", () => {
   it('Testa caso correto 2', () => {
     const use01 = removeVowels("No offense but,\nYour writing is among the worst I've ever read");
     expect(use01).toEqual("N ffns bt,\nYr wrtng s mng th wrst 'v vr rd");
+  });
+});
+
+
+describe("Teste exercício 002", () => {
+  it('Testa caso correto', () => {
+    const use01 = uniqueInOrder('AAAABBBCCDAABBB');
+    expect(use01).toEqual(['A','B','C','D','A','B']);
   });
 });
