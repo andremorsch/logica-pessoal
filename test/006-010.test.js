16dba0989340a78c1120c9606f11ed9e416d230c
André Morsch <andremorsch37@gmail.com>
2022-05-13 17:57:15 -0300

    feat: cria testes para o desafio 007 e 008

--- test/006-010.test.js
+++ test/006-010.test.js
@@ -1,4 +1,6 @@
 const { generateHashtag } = require('../desafios/006-010/006HashtagGenerator');
+const { accum } = require('../desafios/006-010/007AccumLetters');
+const { whatday } = require('../desafios/006-010/008DaysOfTheWeek');
 
 describe("Teste exercício 006", () => {
   it('Testa caso false', () => {
@@ -11,5 +13,33 @@ describe("Teste exercício 006", () => {
     const use02 = generateHashtag("Do We have A Hashtag");
     expect(use02).toEqual("#DoWeHaveAHashtag");
     expect(typeof use02).toEqual("string");
+  });
+});
+
+describe("Testa exercício 007", () => {
+  it('Testa caso correto 01', () => {
+    const use01 = accum("ZpglnRxqenU");
+    expect(use01).toEqual("Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+  });
+
+  it('Testa caso correto 02', () => {
+    const use02 = accum("NyffsGeyylB");
+    expect(use02).toEqual("N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+    expect(typeof use02).toEqual("string");
+  });
+});
+
+describe("Testa exercício 008", () => {
+  it('Testa caso correto 01', () => {
+    const use01 = whatday(1);
+    expect(use01).toEqual("Sunday");
+  });
+  it('Testa caso correto 02', () => {
+    const use01 = whatday(2);
+    expect(use01).toEqual("Monday");
+  });
+  it('Testa caso correto 03', () => {
+    const use01 = whatday(30);
+    expect(use01).toEqual("Wrong, please enter a number between 1 and 7");
   });
 });
